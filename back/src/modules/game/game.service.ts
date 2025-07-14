3a9c026247079b06b7410865790844a4a8ced5ac
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2025-07-14 23:40:13 +0900

    (refactor) 공통함수 분리

--- back/src/modules/game/game.service.ts
+++ back/src/modules/game/game.service.ts
@@ -1,3 +1,4 @@
+import { GameMap } from "../../types/game";
 import {
   canPlaceCard,
   dealCards,
@@ -38,11 +39,9 @@ export class GameService {
     if (this.game.status !== "in-progress")
       throw new Error("플레이 중이 아닙니다");
 
-    const player = this.game.players.find((player) => player.id === playerId);
-    const stack = this.game.stacks.find((stack) => stack.id === stackId);
+    const player = this.findPlayer(playerId);
+    const stack = this.findStack(stackId);
 
-    if (!player) throw new Error("플레이어가 없습니다");
-    if (!stack) throw new Error("스택이 없습니다");
     if (!player.hand.includes(card))
       throw new Error("존재하지 않은 카드입니다");
     if (!canPlaceCard(stack, card)) throw new Error("카드를 올릴 수 없습니다");
@@ -57,5 +56,20 @@ export class GameService {
     );
 
     this.game.dropCardCount = this.game.dropCardCount + 1;
+  }
+
+  findBy<K extends keyof GameMap>(key: K, id: string): GameMap[K] {
+    return this.game[key].find((v) => v.id === id);
+  }
+
+  findStack(stackId: string) {
+    const stack = this.findBy("stacks", stackId);
+    if (!stack) throw new Error("스택이 없습니다");
+    return stack;
+  }
+  findPlayer(playerId: string) {
+    const player = this.findBy("players", playerId);
+    if (!player) throw new Error("플레이어가 없습니다");
+    return player;
   }
 }
--- back/src/types/game.d.ts
+++ back/src/types/game.d.ts
@@ -1,3 +1,8 @@
 import { GAME_STATUS } from "../modules/game/constants";
 
 export type GameStatus = (typeof GAME_STATUS)[keyof typeof GAME_STATUS];
+
+export type GameMap = {
+  players: Player;
+  stacks: Stack;
+};
