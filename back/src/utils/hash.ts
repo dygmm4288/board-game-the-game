6010f7dd7063e5112d3c8e36007d505ebdd61efe
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2025-07-21 23:04:11 +0900

    (update) hash util class

--- back/src/utils/hash.ts
+++ back/src/utils/hash.ts
@@ -1,9 +1,22 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+const JWT_SECRET = process.env.JWT_SECRET!;
 class Hash {
   constructor() {}
 
-  decodeToken() {}
-  generateToken() {}
+  decodeToken(token: string) {
+    return new Promise((res, rej) => {
+      jwt.verify(token, JWT_SECRET, (err, data) => {
+        if (err) return rej(err);
+        return res(data);
+      });
+    });
+  }
+
+  generateToken(payload: object | string, expire?: number) {
+    return jwt.sign(payload, JWT_SECRET, { expiresIn: expire || "10m" });
+  }
+
   generateHash(data: string) {
     return bcrypt.hash(data, 10);
   }
