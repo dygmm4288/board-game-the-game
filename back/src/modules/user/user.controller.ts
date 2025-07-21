4db80741b7be0bfafdf3943c00da215397a2d138
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2025-07-21 22:29:13 +0900

    (update) user route 에러 추가

--- back/src/modules/user/user.controller.ts
+++ back/src/modules/user/user.controller.ts
@@ -3,34 +3,56 @@ import { comparePassword } from "./user.logic";
 import User from "./user.service";
 class UserController {
   public async signUp(req: Request, res: Response) {
-    const { username, password } = req.body;
+    try {
+      const { username, password } = req.body;
 
-    if (!username || !password) return res.status(404);
+      if (!username || !password)
+        return res
+          .status(400)
+          .json({ message: "이름과 비밀번호는 필수입니다" });
 
-    const existUser = await User.getUser(username);
-    if (existUser) return res.status(404);
+      const existUser = await User.getUser(username);
+      if (existUser)
+        return res
+          .status(409)
+          .json({ message: "이미 존재하는 사용자 이름입니다" });
 
-    const user = await User.postUser({ name: username, password });
-    if (!user) res.status(404);
-
-    res.status(200);
+      const user = await User.postUser({ name: username, password });
+      if (!user) throw new Error("서버 오류 발생");
+      res.status(201);
+    } catch (err) {
+      res.status(500).json({ message: "서버 오류 발생", err });
+    }
   }
 
   public async signIn(req: Request, res: Response) {
-    const { username, password } = req.body;
+    try {
+      const { username, password } = req.body;
 
-    const user = await User.getUser(username);
-    if (!user) return res.status(401);
+      if (!username || !password)
+        return res
+          .status(400)
+          .json({ message: "이름과 비밀번호는 필수입니다" });
 
-    if (!(await comparePassword(user.password, password)))
-      return res.status(401);
+      const user = await User.getUser(username);
+      if (!user)
+        return res.status(401).json({ message: "인증에 실패했습니다" });
 
-    const data = {
-      accessToken: "",
-      refreshToken: "",
-    };
+      if (!(await comparePassword(user.password, password)))
+        return res.status(401).json({ message: "인증에 실패했습니다" });
 
-    return res.status(200).json(data);
+      const { password: _, ..._withoutPasswordUser } = user;
+
+      const data = {
+        accessToken: "",
+        refreshToken: "",
+        user: _withoutPasswordUser,
+      };
+
+      return res.status(200).json(data);
+    } catch (err) {
+      res.status(500).json({ message: "서버 오류 발생", err });
+    }
   }
 }
 
