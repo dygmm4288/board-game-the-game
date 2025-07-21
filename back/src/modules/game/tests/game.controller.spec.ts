67c36241c15495f3d2658822428ee875b828e23e
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2025-07-21 21:42:39 +0900

    test code

--- back/src/modules/game/tests/game.controller.spec.ts
+++ back/src/modules/game/tests/game.controller.spec.ts
@@ -1,6 +1,8 @@
 import express, { Application } from "express";
 import request from "supertest";
 import gameController from "../game.controller";
+import { Game } from "../game.model";
+import { GameService } from "../game.service";
 
 const app: Application = express();
 
@@ -15,8 +17,15 @@ describe("POST /games", () => {
   });
 
   it("요청이 들어오면 올바르게 게임을 만들어서 반환해야 한다.", async () => {
-    const response = await request(app).post("/games");
+    const fakeGame: Partial<Game> = {
+      id: "test-game",
+      players: [{ id: "p1", name: "p1", hand: [] }],
+    };
 
+    (GameService.setupNewGame as jest.Mock).mockReturnValue(fakeGame);
+
+    const response = await request(app).post("/games");
     expect(response.status).toBe(200);
+    expect(response.body).toEqual(fakeGame);
   });
 });
