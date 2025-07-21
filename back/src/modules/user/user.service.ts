f72c47c3f89f1d92cb9ddc60a00ca713f3e0f5d9
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2025-07-21 22:21:06 +0900

    (update) user service repository로 변경

--- back/src/modules/user/user.service.ts
+++ back/src/modules/user/user.service.ts
@@ -1,10 +1,29 @@
+import { Repository } from "typeorm";
+import { AppDataSource } from "../../config/db";
 import { UserModel } from "./user.model";
 
-export const getUser = async (
-  username: string,
-): Promise<UserModel | null> => {};
+export class UserService {
+  private userRepository: Repository<UserModel>;
 
-export const postUser = async (
-  username: string,
-  password: string,
-): Promise<UserModel | null> => {};
+  constructor() {
+    this.userRepository = AppDataSource.getRepository(UserModel);
+  }
+
+  async postUser(
+    userData: Pick<UserModel, "name" | "password">,
+  ): Promise<UserModel> {
+    const newUser = this.userRepository.create({
+      name: userData.name,
+      password: userData.password,
+    });
+
+    await this.userRepository.save(newUser);
+
+    return newUser;
+  }
+
+  async getUser(name: string): Promise<UserModel | null> {
+    const user = await this.userRepository.findOneBy({ name });
+    return user;
+  }
+}
