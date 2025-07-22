db182d673974fd5440a43642c4a73bca9e2a9674
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2025-07-22 21:41:33 +0900

    (add) use auth hook

---
+++ front/src/hooks/useAuth.ts
@@ -0,0 +1,5 @@
+export default function useAuth() {
+  const isAuth = true;
+
+  return { isAuth };
+}
--- front/src/routes/ProtectedRoute.tsx
+++ front/src/routes/ProtectedRoute.tsx
@@ -1,5 +1,10 @@
 import type { PropsWithChildren } from "react";
+import { Navigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 export default function ProtectedRoute({ children }: PropsWithChildren) {
+  const { isAuth } = useAuth();
+
+  if (!isAuth) return <Navigate to='/login' />;
   return <>{children}</>;
 }
