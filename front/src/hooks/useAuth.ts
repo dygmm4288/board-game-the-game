3039cf5ca8b742f74fc664b65f69f29697913417
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2025-07-22 21:44:15 +0900

    (add) zustand and use Auth

--- front/src/hooks/useAuth.ts
+++ front/src/hooks/useAuth.ts
@@ -1,5 +1,14 @@
+import { create } from "zustand";
+
+interface AuthState {
+  isAuth: boolean;
+}
+const useAuthStore = create<AuthState>()(() => ({
+  isAuth: false,
+}));
+
 export default function useAuth() {
-  const isAuth = true;
+  const { isAuth } = useAuthStore();
 
   return { isAuth };
 }
