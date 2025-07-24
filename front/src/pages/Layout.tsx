9e4f3c04ac21662ac7c9df2105bc380c820bcbf2
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2025-07-24 22:10:14 +0900

    game front

--- front/src/pages/Layout.tsx
+++ front/src/pages/Layout.tsx
@@ -1,8 +1,9 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
     <section>
+      <Link to='/game'>게임</Link>
       <Outlet />
     </section>
   );
