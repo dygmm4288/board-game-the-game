c15ef53055eef008d26e93b257d9c6c04c59d095
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2025-07-27 22:03:45 +0900

    (update) Game 클래스에 id 속성 추가 및 생성자 수정

--- front/src/models/game.ts
+++ front/src/models/game.ts
@@ -3,6 +3,7 @@ import type Stack from "./game copy";
 import type User from "./user";
 
 export default class Game extends Base {
+  id: string;
   status: "waiting" | "in-progress" | "finished";
   deck: number[];
   stacks: Stack[];
@@ -12,6 +13,7 @@ export default class Game extends Base {
 
   constructor(data: Game) {
     super();
+    this.id = data.id;
     this.status = data.status;
     this.deck = data.deck;
     this.stacks = data.stacks;
