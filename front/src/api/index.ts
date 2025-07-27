2966c592c4138293490d53e5f20d088da52f95e4
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2025-07-27 22:24:45 +0900

    (refactor)  인증 헤더 추가 및 401 에러 처리 함수 분리

--- front/src/api/index.ts
+++ front/src/api/index.ts
@@ -1,16 +1,32 @@
-import axios from "axios";
+import axios, { AxiosError, type InternalAxiosRequestConfig } from "axios";
 import store, { ACCESS_TOKEN_KEY, USER_KEY } from "../utils/store";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
+const attachAuthHeader = (config: InternalAxiosRequestConfig) => {
+  const token = store.load(ACCESS_TOKEN_KEY);
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+};
+
+const redirectOnUnauthorized = (error: AxiosError) => {
+  if (error.response && error.response.status === 401) {
+    const nextPath = encodeURIComponent(
+      window.location.pathname + window.location.search,
+    );
+
+    store.delete(ACCESS_TOKEN_KEY);
+    store.delete(USER_KEY);
+
+    window.location.href = `/login?nextPath=${nextPath}`;
+  }
+};
+
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = store.load(ACCESS_TOKEN_KEY);
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
+    config = attachAuthHeader(config);
 
     return config;
   },
@@ -20,16 +36,7 @@ axiosInstance.interceptors.request.use(
 axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response && error.response.status === 401) {
-      const nextPath = encodeURIComponent(
-        window.location.pathname + window.location.search,
-      );
-
-      store.delete(ACCESS_TOKEN_KEY);
-      store.delete(USER_KEY);
-
-      window.location.href = `/login?nextPath=${nextPath}`;
-    }
+    redirectOnUnauthorized(error);
 
     return Promise.reject(error);
   },
