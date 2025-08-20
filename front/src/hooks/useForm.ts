f49e9d61c14548ebcfdbefb99e8091aaefedeaf0
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2025-08-20 22:13:46 +0900

    feat: useform 더 작게 수정

--- front/src/hooks/useForm.ts
+++ front/src/hooks/useForm.ts
@@ -5,14 +5,12 @@ type Keyof<T extends Obj> = Extract<keyof T, string>;
 
 type useFormReturn<T extends Obj> = {
   value: Readonly<T>;
-  get: <K extends Keyof<T>>(key: K) => T[K];
   set: <K extends Keyof<T>>(key: K, newValue: T[K]) => void;
 };
 
 const useForm = <T extends Obj>(defaultValue: T): useFormReturn<T> => {
   const [value, setValue] = useState(defaultValue);
 
-  const get = <K extends Keyof<T>>(key: K) => value[key];
   const set = <K extends Keyof<T>>(key: K, newValue: T[K]) => {
     setValue((prev) =>
       prev[key] === newValue ? prev : { ...prev, [key]: newValue },
@@ -21,7 +19,6 @@ const useForm = <T extends Obj>(defaultValue: T): useFormReturn<T> => {
 
   return {
     value,
-    get,
     set,
   };
 };
